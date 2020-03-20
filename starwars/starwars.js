2fae92807020c31dcd1b6e62f79e1b60149e6b66
chrismerc95 <61207519+chrismerc95@users.noreply.github.com>
2020-03-20 02:47:02 -0600

    updated commit

---
+++ starwars/starwars.js
@@ -0,0 +1,61 @@
+import { films } from '../data/films.js'
+import { people } from '../data/people.js'
+
+let gallery = document.querySelector('.gallery')
+
+const maleCharacters = people.filter(person => person.gender === "male")
+
+const femaleCharacters = people.filter(person => person.gender === "female")
+
+const otherCharacters = people.filter(person => person.gender === "n/a" || person.gender === "none" || person.gender === "hermaphrodite")
+
+let maleButton = document.querySelector('#maleButton')
+let femaleButton = document.querySelector('#femaleButton')
+let otherButton = document.querySelector('#otherButton')
+
+maleButton.addEventListener("click", function( event ) {
+    populateDOM(maleCharacters)
+})
+
+femaleButton.addEventListener("click", function( event ) {
+    populateDOM(femaleCharacters)
+})
+
+otherButton.addEventListener("click", function( event ) {
+    populateDOM(otherCharacters)
+})
+
+function getLastNumber(url) {
+    let end = url.lastIndexOf('/') 
+    let start = end - 2
+    if (url.charAt(start) === '/') {
+        start++
+    }
+    return url.slice(start,end)
+    }
+    
+    function removeChildren(element) {
+        while (element.firstChild) {
+            element.removeChild(element.firstChild)
+        }
+    }
+
+function populateDOM(characters) {
+    removeChildren(gallery)
+    characters.forEach(person => {  
+        let imageNum = getLastNumber(person.url)
+        let personAnchor = document.createElement("a")
+        personAnchor.href = "#"
+        let personImg = document.createElement("img")
+        personImg.src = `https://starwars-visualguide.com/assets/img/characters/${imageNum}.jpg`
+
+        personImg.addEventListener("error", (event) => {
+        personImg.hidden = true 
+        })
+
+        personAnchor.appendChild(personImg)
+        gallery.appendChild(personAnchor)
+    })
+}
+
+populateDOM(people)
